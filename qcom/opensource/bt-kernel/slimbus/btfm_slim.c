bf90a6254d88b3a3cae9edcb311f8b96fb156114
zaraknyu <razenxizu@gmail.com>
2026-07-03 04:50:54 +0000

    Update 'qcom/opensource/bt-kernel/' from TAG MMI-W1WAA36.48-12-40
    Signed-off-by: zaraknyu <razenxizu@gmail.com>

--- qcom/opensource/bt-kernel/slimbus/btfm_slim.c
+++ qcom/opensource/bt-kernel/slimbus/btfm_slim.c
@@ -177,7 +177,6 @@ int btfm_slim_disable_ch(struct btfmslim *btfmslim, struct btfmslim_ch *ch,
 {
 	int ret = -1;
 	int i = 0;
-	struct btfmslim_ch *chan = ch;
 	int chipset_ver = 0;
 	if (!btfmslim || !ch)
 		return -EINVAL;
@@ -216,9 +215,9 @@ int btfm_slim_disable_ch(struct btfmslim *btfmslim, struct btfmslim_ch *ch,
 		BTFMSLIM_ERR("slim_stream_unprepare failed returned val = %d", ret);
 
 	/* Disable port through registration setting */
-	for (i = 0; i < nchan; i++, chan++) {
+	for (i = 0; i < nchan; i++, ch++) {
 		if (btfmslim->vendor_port_en) {
-			ret = btfmslim->vendor_port_en(btfmslim, chan->port,
+			ret = btfmslim->vendor_port_en(btfmslim, ch->port,
 				rxport, 0);
 			if (ret < 0) {
 				BTFMSLIM_ERR("vendor_port_en failed [%d]", ret);
@@ -237,10 +236,7 @@ int btfm_slim_disable_ch(struct btfmslim *btfmslim, struct btfmslim_ch *ch,
 	if (btfm_num_ports_open > 0)
 		btfm_num_ports_open--;
 
-	if (ch->dai.sruntime != NULL) {
-		slim_stream_free(ch->dai.sruntime);
-		ch->dai.sruntime = NULL;
-	}
+	ch->dai.sruntime = NULL;
 
 	BTFMSLIM_INFO("btfm_num_ports_open: %d", btfm_num_ports_open);
 
